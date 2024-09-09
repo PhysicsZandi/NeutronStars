033e1043f719449ac3b42c802956f5e43ec0a77e
PhysicsZandi <physicszandi@gmail.com>
2024-09-09 12:30:06 +0200

    add physical constants

---
+++ src/physical_constants.py
@@ -0,0 +1,25 @@
+# Physical constants in cgs
+
+c = 2.99792458e10  # cm
+h = 6.6260755e-27  # erg s
+hbar = 1.05457266e-27  # erg s
+G = 6.67259e-8  # g^−1 cm3 s^−2 or erg cm g^−2
+m_e = 9.1093897e-28  # g
+m_p = 1.6726231e-24  # g
+m_n = 1.6749286e-24  # g
+eV = 1.6021772e-12  # erg
+
+m_sun = 1.9884e33  # g
+km = 1e5  # cm
+
+# Physical constants in MeV-fm
+
+m_e_MeV = 0.511  # MeV c^-2
+m_p_MeV = 938.272  # Mev c^-2
+m_n_MeV = 939.566  # Mev c^-2
+hc_MeV = 197.3  # MeV fm
+
+# Conversion between MeV-fm and cgs
+
+MeV = 1e6 * eV  # erg
+fm = 1e-13  # cm
