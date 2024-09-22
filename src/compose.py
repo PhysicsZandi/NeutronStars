1b109a7109a41cf59c18c51fe73a8a5caf58440d
PhysicsZandi <physicszandi@gmail.com>
2024-09-22 20:24:34 +0200

    update compose part

--- src/compose.py
+++ src/compose.py
@@ -18,7 +18,7 @@ def all_eos():
         "nucleonic_models": {"label": "Nucleonic", "color": "violet", "plotted": False},
     }
 
-    folder_path = "/Users/matte/projects/TOV/src/compose_eos"
+    folder_path = "compose_eos"
 
     for root, dirs, files in os.walk(folder_path):
         for file_name in files:
@@ -36,7 +36,6 @@ def all_eos():
                         n, p, e = line.strip().split(",")
                         pressures.append(float(p))
                         energy_densities.append(float(e))
-
                 # Plot equation of state with color according to the folder
                 folder_info = folder_labels[folder_name]
                 if not folder_info["plotted"]:
@@ -71,7 +70,7 @@ def all_MvsR(resolution=1e3, n_points=40):
 
     range_initial_pressures = np.logspace(33, 36, n_points)
 
-    folder_path = "/Users/matte/projects/TOV/src/compose_eos"
+    folder_path = "compose_eos"
 
     for root, dirs, files in os.walk(folder_path):
         for file_name in files:
@@ -104,6 +103,81 @@ def all_MvsR(resolution=1e3, n_points=40):
                     folder_info["plotted"] = True
                 else:
                     plt.plot(r, m, color=folder_info["color"])
+
+    plt.xlabel("Radius (km)")
+    plt.ylabel("Mass ($M_{\\odot}$)")
+    plt.legend()
+    plt.show()
+
+
+# Plot all the equation of state
+# in folder given by folder_initial_letters and name given by file_initial_letters
+def all_eos_v2(file_initial_letters="", folder_initial_letters=""):
+    folder_path = "compose_eos"
+
+    for root, dirs, files in os.walk(folder_path):
+        for file_name in files:
+            # Find only files with csv extension
+            if file_name.endswith(".csv"):
+                file_path = os.path.join(root, file_name)
+                base_name = os.path.splitext(file_name)[0]
+                folder_name = os.path.basename(root)
+
+                if file_name.startswith(
+                    file_initial_letters
+                ) and folder_name.startswith(folder_initial_letters):
+                    # Load the equation of state
+                    pressures = []
+                    energy_densities = []
+                    with open(file_path, "r") as f:
+                        next(f)
+                        for line in f:
+                            n, p, e = line.strip().split(",")
+                            pressures.append(float(p))
+                            energy_densities.append(float(e))
+
+                    plt.plot(pressures, energy_densities, label=base_name)
+
+    plt.ylabel("Energy density ($erg / cm^3$)")
+    plt.yscale("log")
+    plt.xlabel("Pressure ($dyne / cm^2$)")
+    plt.xscale("log")
+    plt.legend()
+    plt.show()
+
+
+# Plot all the mass versus radius plots
+# in folder given by folder_initial_letters and name given by file_initial_letters
+def all_MvsR_v2(
+    resolution=1e3, n_points=40, file_initial_letters="", folder_initial_letters=""
+):
+    range_initial_pressures = np.logspace(33, 36, n_points)
+
+    folder_path = "compose_eos"
+
+    for root, dirs, files in os.walk(folder_path):
+        for file_name in files:
+            # Find only files with csv extension
+            if file_name.endswith(".csv"):
+                file_path = os.path.join(root, file_name)
+                base_name = os.path.splitext(file_name)[0]
+                folder_name = os.path.basename(root)
+
+                if file_name.startswith(
+                    file_initial_letters
+                ) and folder_name.startswith(folder_initial_letters):
+                    # Compute mass and radius of the object
+                    eos = interpolation_eos(file_path, low_density_eos=True)
+                    solver = Solver_range(
+                        eos,
+                        resolution,
+                        range_initial_pressures,
+                        relativity_corrections=True,
+                    )
+                    solver.solve()
+                    r, m = solver.get()
+
+                    plt.plot(r, m, label=base_name)
 
     plt.xlabel("Radius (km)")
     plt.ylabel("Mass ($M_{\\odot}$)")
